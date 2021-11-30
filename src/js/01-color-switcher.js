af17e10f5c383aeece62e1358465aebe2517770b
Vladymyr <85882951+Syglob@users.noreply.github.com>
2021-11-30 16:52:27 +0200

    1st task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+timer = null;
+const btnStart = document.querySelector('[data-start]');
+const btnStop = document.querySelector('[data-stop]');
 
+//
+btnStart.addEventListener('click', randomPageColor);
+btnStop.addEventListener('click', () => {
+  clearInterval(timer);
+  if (btnStop) {
+    btnStart.disabled = false;
+  }
+});
+
+//
+
+function randomPageColor() {
+  timer = setInterval(() => {
+    const color = getRandomHexColor();
+    document.body.style.backgroundColor = color;
+  }, 1000);
+  if (btnStart) {
+    btnStart.disabled = true;
+  }
+}
